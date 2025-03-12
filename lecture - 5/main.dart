395c741a2512ba7f75f1419a5b92a54771ace649
Drako-Rexon <abhishek491981@gmail.com>
2025-03-13 01:15:06 +0530

    lecture 5

---
+++ lecture - 5/main.dart
@@ -0,0 +1,37 @@
+void main() {
+  int answer = 3;
+  //= squareOfANumber(3);
+  print("the square of 3 is: "+answer.toString());
+}
+
+
+
+int squareOfANumber(int number) {
+  int square = number * number;
+  return square;
+}
+
+// functions are the reusable code snippet, jisko ham repeatedly use krte hai
+// apne code ko simplify and less length bnate hai
+
+
+// -> single line comment
+
+
+/* 
+ * 
+ * multi line comment
+ * 
+ * 
+ */
+
+
+
+/*
+// syntax:
+
+// return_type function_name( arguments ) {
+//     statements
+// }
+
+*/
