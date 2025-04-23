f46eb660b453a5773acdf39d82076edb71038f18
Drako-Rexon <abhishek491981@gmail.com>
2025-04-24 02:09:24 +0530

    lecture 10 done

---
+++ lecture - 10/main.dart
@@ -0,0 +1,32 @@
+void printCount(int number) {
+//   for (int i=0 ; i<number; i++) {
+//     print(i+1);
+//   }
+  
+  // 1. base condition
+  if (number == 0) return;
+  
+  // 2. recursively function call
+  printCount(number-1);
+  print(number);
+  
+  // 3. returning the answer
+  return;
+}
+
+
+int factorial(int num) {
+  if (num == 0) return 1;
+  
+  int answer = num * factorial(num - 1); // 120 in case of 5
+  
+  return answer;
+}
+
+
+
+void main() {
+  printCount(10);
+  
+  print("The factorial of 5 is: "+ factorial(6).toString());
+}
